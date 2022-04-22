b72bc84ae583bc54ad7d2921851f46a6869e1547
rreynaldo <rreynaldo@users.noreply.github.com>
2022-04-22 15:09:49 +0000

    Tokens updated

--- build/ios/StyleDictionaryColor.h
+++ build/ios/StyleDictionaryColor.h
@@ -4,7 +4,7 @@
 //
 
 // Do not edit directly
-// Generated on Thu, 16 Dec 2021 18:06:29 GMT
+// Generated on Fri, 22 Apr 2022 15:09:48 GMT
 
 
 #import <UIKit/UIKit.h>
@@ -16,6 +16,7 @@ ColorColorsSingleBlue,
 ColorColorsRefBlue,
 ColorColorsSpecialCharacters,
 ColorColorsSpecialCharactersNderung,
+ColorInvalidEmpty,
 ColorLightBackground,
 ColorDarkBackground
 };
